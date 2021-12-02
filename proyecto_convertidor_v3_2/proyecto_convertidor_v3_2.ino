3900084f73e7e6852da63daa425ef60c9035dad0
Ignacio Gamboa <ign.gga@outlook.cl>
2021-12-02 14:59:28 -0300

    code pulentoso-oso

--- proyecto_convertidor_v3_2/proyecto_convertidor_v3_2.ino
+++ proyecto_convertidor_v3_2/proyecto_convertidor_v3_2.ino
@@ -5,11 +5,11 @@ int M_pwm1 = 2;
 int M_pwm3 = 4;
 int M_pwm5 = 7;
 int const frecuencia = 1;
-const float periodo = (1/frecuencia);
-const float timer = (periodo*1000)/2;
-char comando=Serial.read();
+const float periodo = (1 / frecuencia);
+const float timer = (periodo * 1000) / 2;
+
 void setup()
-{   
+{
   Serial.begin(9600);
   pinMode(P_pwm1, OUTPUT);
   pinMode(P_pwm3, OUTPUT);
@@ -19,47 +19,47 @@ void setup()
   pinMode(M_pwm5, OUTPUT);
 }
 
-void pwm1On(){
-  digitalWrite(P_pwm1,1);
-  digitalWrite(M_pwm1,0);
+void pwm1On() {
+  digitalWrite(P_pwm1, 1);
+  digitalWrite(M_pwm1, 0);
 }
-void pwm1Off(){
-  digitalWrite(P_pwm1,0);
-  digitalWrite(M_pwm1,1);
+void pwm1Off() {
+  digitalWrite(P_pwm1, 0);
+  digitalWrite(M_pwm1, 1);
 }
-void pwm3On(){
-  digitalWrite(P_pwm3,1);
-  digitalWrite(M_pwm3,0);
+void pwm3On() {
+  digitalWrite(P_pwm3, 1);
+  digitalWrite(M_pwm3, 0);
 }
-void pwm3Off(){
-  digitalWrite(P_pwm3,0);
-  digitalWrite(M_pwm3,1);
+void pwm3Off() {
+  digitalWrite(P_pwm3, 0);
+  digitalWrite(M_pwm3, 1);
 }
-void pwm5On(){
-  digitalWrite(P_pwm5,1);
-  digitalWrite(M_pwm5,0);
+void pwm5On() {
+  digitalWrite(P_pwm5, 1);
+  digitalWrite(M_pwm5, 0);
 }
-void pwm5Off(){
-  digitalWrite(P_pwm5,0);
-  digitalWrite(M_pwm5,1);
+void pwm5Off() {
+  digitalWrite(P_pwm5, 0);
+  digitalWrite(M_pwm5, 1);
 }
 //=======================================
 //PWM1 y PWM5 siempre han de ser OPUESTOS
 //O complementarios entre si
 //-------------------
-void signalMed1(){//|
+void signalMed1() { //|
   pwm1On();       //|
   pwm5Off();      //|
 }                 //|
-void signalMed2(){//|
-    pwm1Off();    //|
-    pwm5On();     //|
+void signalMed2() { //|
+  pwm1Off();    //|
+  pwm5On();     //|
 }                 //|
 //-------------------
 //=======================================
 //señal 3 igual a señal 1, 1 y 5 opuestas
-void signalForward(){
-  for (;;){
+void signalForward() {
+  for (;;) {
     signalMed1();
     pwm3On();
     delay(timer);
@@ -70,8 +70,8 @@ void signalForward(){
 }
 //========================================
 //señal 3 igual a señal 5, 1 y 5 opuestas
-void signalReverse(){
-  for(;;){
+void signalReverse() {
+  for (;;) {
     signalMed1();
     pwm3Off();
     delay(timer);
@@ -82,9 +82,9 @@ void signalReverse(){
 }
 //========================================
 //señal 3 siempre en alto, 1 y 5 opuestas
-void signalOut(){
+void signalOut() {
   pwm3On();
-  for(;;){
+  for (;;) {
     signalMed1();
     delay(timer);
     signalMed2();
@@ -95,17 +95,45 @@ void signalOut(){
 //PWM1==>Controla OPTO 2
 //PWM3==>Controla OPTO 1
 //PWM5==>Controla OPTO 3
-void loop(){
+void loop() {
   pwm1Off();
   pwm3Off();
   pwm5Off();
-  Serial.print("/n/t/tSeleccione que señal desea generar:/n");
-  do{
-    signalForward();
+  Serial.print("\n\t\tSeleccione que señal desea generar:\nSeñal 1:\tPWM1==PMW3!=PWM5\nSeñal 2:\tPWM5==PMW3!=PWM1\nSeñal 3:\tPWM3=Constante y PMW1!=PWM5");
+  do {
+    if (Serial.available() > 0) {
+      int comando = Serial.read();
+      switch (comando) {
+        case '1':
+          do {
+            Serial.print("\nSeñal 1:\nPWM1 y PWM5 complementarios,\nY PWM3 == PWM1\n");
+            signalForward();
+          }
+          while (true);
+          break;
+        case '2':
+          do {
+            Serial.print("\nSeñal 1:\nPWM1 y PWM5 complementarios,\nY PWM3 == PWM5\n");
+            signalReverse();
+          }
+          while (true);
+          break;
+        case '3':
+          do {
+            Serial.print("\nSeñal 1:\nPWM1 y PWM5 complementarios,\nY PWM3 == HIGH\n");
+            signalOut();
+          }
+          while (true);
+          break;
+        case '0':
+          do {
+            Serial.print("\n\t\tStop\n");
+            exit(0);
+          }
+          while (true);
+          break;
+      }
+    }
   }
-  while(comando==1);
-  do{
-    signalReverse();
-  }
-  while(comando==2);
+  while(true);
 }
