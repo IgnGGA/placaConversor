95c1645ce22c0f0477aac27217fd49a0507729b4
Ignacio Gamboa <ign.gga@outlook.cl>
2021-12-03 11:36:44 -0300

    se modifica el programa para verificar el puerto donde se conecta, y verificar si la señal seleccionada es valida

--- proyecto_convertidor_v3_2/proyectoConvertidor.py
+++ proyecto_convertidor_v3_2/proyectoConvertidor.py
@@ -5,31 +5,48 @@ import sys
 
 
 def proyectoConvertidor():
-    pulsos = serial.Serial('COM4', baudrate=9600, timeout=1)
-    print("\nFrecuencia = 1Hz por sistema, su moidificacion es directamente en el codigo fuente\n")
-    print("\n\t\tSeleccione que señal desea generar:\nSeñal 1:\tPWM1==PMW3!=PWM5\nSeñal 2:\tPWM5==PMW3!=PWM1\nSeñal 3:\tPWM3=Cte y PMW1!=PWM5\nSeñal 4:\tPWM1==PWM3==PWM5\nSeñal 5:\tPWM1==PWM5 y PWM3==Cte")
-    def signalsArduino():
-        selector = input()
-        if (selector == '1'):
-            print("\nSeñal 1:\nPWM1 y PWM5 complementarios,\nY PWM3 == PWM1\n")
-        elif (selector == '2'):
-            print("\nSeñal 2:\nPWM1 y PWM5 complementarios,\nY PWM3 == PWM5\n")
-        elif (selector == '3'):
-            print("\nSeñal 3:\nPWM1 y PWM5 complementarios,\nY PWM3 == HIGH\n")
-        elif (selector == '4'):
-            print("\nSeñal 4:\nPWM1==PWM3==PWM5")
-        elif (selector == '5'):
-            print("\nSeñal 4:\nPWM1==PWM5 y PWM3==Cte")
-        elif (selector == '0'):
-            print("\n\t\tStop\n")
-        while(True):
-            pulsos.write(selector.encode("utf-8"))
-            mensajeARD = pulsos.readline()
-            converMsj = str(mensajeARD.decode("utf-8"))
-            confirmacion = converMsj.strip()
-            print("{}".format(confirmacion))
-            print("\n\tPara seleccionar otro pulso, reiniciar placa manualmente\ncon boton RESET")
-            signalsArduino()
-            break
-    signalsArduino()
+    print("Seleccionar puerto de conección de tarjeta para pruebas Arduino")
+    while True:
+        try:
+            numPort=input("Numero de Puerto:\t")
+            intNumPort=int(numPort)
+            if (intNumPort==int(numPort)):
+                pulsos = serial.Serial('COM{}'.format(intNumPort), baudrate=9600, timeout=1)
+                print("\nFrecuencia = 1Hz por sistema, su moidificacion es directamente en el codigo fuente de la placa Arduino\n")
+                print("\n\t\tSeleccione que señal desea generar:\nSeñal 1:\tPWM1==PMW3!=PWM5\nSeñal 2:\tPWM5==PMW3!=PWM1\nSeñal 3:\tPWM3=Cte y PMW1!=PWM5\nSeñal 4:\tPWM1==PWM3==PWM5\nSeñal 5:\tPWM1==PWM5 y PWM3==Cte")
+                def signalsArduino():
+                    selector = input("Numero de Señal:\t")
+                    listSignals=('0','1','2','3','4','5')
+                    if (selector in listSignals and selector == '1'):
+                        print("\nSeñal 1:\nPWM1 y PWM5 complementarios,\nY PWM3 == PWM1\n")
+                    elif (selector in listSignals and selector == '2'):
+                        print("\nSeñal 2:\nPWM1 y PWM5 complementarios,\nY PWM3 == PWM5\n")
+                    elif (selector in listSignals and selector == '3'):
+                        print("\nSeñal 3:\nPWM1 y PWM5 complementarios,\nY PWM3 == HIGH\n")
+                    elif (selector in listSignals and selector == '4'):
+                        print("\nSeñal 4:\nPWM1==PWM3==PWM5")
+                    elif (selector in listSignals and selector == '5'):
+                        print("\nSeñal 4:\nPWM1==PWM5 y PWM3==Cte")
+                    elif (selector in listSignals and selector == '0'):
+                        print("\n\t\tStop\n")
+                    while(True):
+                        if (selector in listSignals):
+                            pulsos.write(selector.encode("utf-8"))
+                            mensajeARD = pulsos.readline()
+                            converMsj = str(mensajeARD.decode("utf-8"))
+                            confirmacion = converMsj.strip()
+                            print("{}".format(confirmacion))
+                            print("\n\tPara seleccionar otro pulso, REINICIAR placa manualmente\n\tcon boton RESET\n\n")
+                            signalsArduino()
+                            break
+                        elif (selector not in listSignals):
+                            print("Señal seleccionada INVALIDA, favor de seleccionar OTRA señal.\n")
+                            signalsArduino()
+                            break
+                        break
+                signalsArduino()
+                break
+        except ValueError:
+            print('Ingrese un puerto valido')
+
 proyectoConvertidor()
