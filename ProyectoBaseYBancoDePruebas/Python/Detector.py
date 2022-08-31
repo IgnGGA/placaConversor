555334cd3948f44962bc224c2074c55587a52aa9
Ignacio Gamboa <ign.gga@outlook.cl>
2022-08-31 14:23:24 -0400

    se agregan mensajes nuevos

--- ProyectoBaseYBancoDePruebas/Python/Detector.py
+++ ProyectoBaseYBancoDePruebas/Python/Detector.py
@@ -4,6 +4,7 @@ def DetectandoBancoDePruebas():
     from Mensajes import mensajesConectando as conexion
     from Mensajes import mensajeErrorConectando as errorConexion
     from Mensajes import mensajesEsperados as sincro
+    from Mensajes import cierresDePrograma as cierre
     import Conectando as conect
     try:
         bancoDePruebas=serial.Serial('COM{}'.format(conect.conexionPC()),baudrate=9600,timeout=5)
@@ -16,8 +17,13 @@ def DetectandoBancoDePruebas():
             bancoDePruebas.write(sincro("sincronizadorDos"))
             if PL(Lectura(),sincro("OK"))==True:
                 sincro("Activado")
+            else:
+                cierre(3)
+                bancoDePruebas.close()
         else:
             errorConexion(4)
+            cierre(2)
+            bancoDePruebas.close()
         bancoDePruebas.close()
     except serial.serialutil.SerialException:
         errorConexion(4)
